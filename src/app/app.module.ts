994b8fdaccbcb759cc2470543089163a2ba622d1
GDoldurov <veseliaka_007@abv.bg>
2017-11-04 10:53:01 +0000

    end of exercise-with error in compile star.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { StarComponent } from './shared/star.component';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -8,7 +9,8 @@ import { ProductListComponent } from './products/product-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
